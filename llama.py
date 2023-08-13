f1d227bf559d3c23740cd34935c39aca8e3b00c7
Vui Seng Chua <vui.seng.chua@intel.com>
2023-08-13 16:24:55 -0700

    Enhance --torch_profile of llama.py

--- llama.py
+++ llama.py
@@ -143,7 +143,9 @@ def load_quant(model, checkpoint, wbits):
 
 
 # function for benchmarking runtime
-def benchmark(model, input_ids, check=False):
+def benchmark(model, input_ids, check=False, torchprof=False):
+    from torch.profiler import profile, record_function, ProfilerActivity
+
     input_ids = input_ids.to(model.gpus[0] if hasattr(model, 'gpus') else DEV)
     torch.cuda.synchronize()
 
@@ -168,31 +170,76 @@ def benchmark(model, input_ids, check=False):
                 torch.cuda.synchronize(gpu)
         else:
             torch.cuda.synchronize()
-    max_memory = 0
-    with torch.no_grad():
-        attention_mask = torch.ones((1, input_ids.numel()), device=DEV)
-        times = []
-        for i in range(input_ids.numel()):
-            tick = time.time()
-            out = model(
-                input_ids[:, i:i+1],
-                past_key_values=cache['past'],
-                attention_mask=attention_mask[:, :(i + 1)].reshape((1, -1))
-            )
+
+    if torchprof is True:
+        def profiler_trace_handler(p):
+            p_avg = p.key_averages()
+            print(p_avg.table(top_level_events_only=True))
+            print("JOTO")
+            # table()        Args:
+            # sort_by (str, optional): Attribute used to sort entries. By default
+            #     they are printed in the same order as they were registered.
+            #     Valid keys include: ``cpu_time``, ``cuda_time``, ``cpu_time_total``,
+            #     ``cuda_time_total``, ``cpu_memory_usage``, ``cuda_memory_usage``,
+            #     ``self_cpu_memory_usage``, ``self_cuda_memory_usage``, ``count``.
+            # top_level_events_only(bool, optional): Boolean flag to determine the
+            #     selection of events to display. If true, the profiler will only
+            #     display events at top level like top-level invocation of python
+            #     `lstm`, python `add` or other functions, nested events like low-level
+            #     cpu/cuda ops events are omitted for profiler result readability.
+
+        with profile(
+            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], 
+            record_shapes=True,
+            profile_memory=True, 
+            on_trace_ready=profiler_trace_handler) as prof:
+            with torch.no_grad():
+                attention_mask = torch.ones((1, input_ids.numel()), device=DEV)
+
+                with record_function(f"generate_{input_ids.numel()}_token"):
+                    for i in range(input_ids.numel()):
+                        out = model(
+                            input_ids[:, i:i+1],
+                            past_key_values=cache['past'],
+                            attention_mask=attention_mask[:, :(i + 1)].reshape((1, -1))
+                        )
+                        sync()
+
+                        if check and i != input_ids.numel() - 1:
+                            tot += loss(out.logits[0].to(DEV), input_ids[:, (i + 1)].to(DEV)).float()
+                        cache['past'] = list(out.past_key_values)
+                        del out
+                    sync()
+
+                if check:
+                    print('PPL:', torch.exp(tot / (input_ids.numel() - 1)).item())
+
+    else:
+        max_memory = 0
+        with torch.no_grad():
+            attention_mask = torch.ones((1, input_ids.numel()), device=DEV)
+            times = []
+            for i in range(input_ids.numel()):
+                tick = time.time()
+                out = model(
+                    input_ids[:, i:i+1],
+                    past_key_values=cache['past'],
+                    attention_mask=attention_mask[:, :(i + 1)].reshape((1, -1))
+                )
+                sync()
+                times.append(time.time() - tick)
+                print(i, times[-1])
+                max_memory = max(max_memory,torch.cuda.memory_allocated() / 1024 /1024)
+                if check and i != input_ids.numel() - 1:
+                    tot += loss(out.logits[0].to(DEV), input_ids[:, (i + 1)].to(DEV)).float()
+                cache['past'] = list(out.past_key_values)
+                del out
             sync()
-            times.append(time.time() - tick)
-            print(i, times[-1])
-            max_memory = max(max_memory,torch.cuda.memory_allocated() / 1024 /1024)
-            if check and i != input_ids.numel() - 1:
-                tot += loss(out.logits[0].to(DEV), input_ids[:, (i + 1)].to(DEV)).float()
-            cache['past'] = list(out.past_key_values)
-            del out
-        sync()
-        import numpy as np
-        print('Median:', np.median(times))
-        if check:
-            print('PPL:', torch.exp(tot / (input_ids.numel() - 1)).item())
-            print('max memory(MiB):',max_memory)
+            import numpy as np
+            print('Median:', np.median(times))
+            if check:
+                print('PPL:', torch.exp(tot / (input_ids.numel() - 1)).item())
+                print('max memory(MiB):',max_memory)
 
 if __name__ == '__main__':
     import argparse
@@ -268,19 +315,7 @@ if __name__ == '__main__':
         model = model.to(DEV)
         if args.benchmark:
             input_ids = next(iter(dataloader))[0][:, :args.benchmark]
-
-            if args.torch_profile:
-                from torch.profiler import profile, record_function, ProfilerActivity
-                with torch.profiler.profile(
-                activities=[
-                   torch.profiler.ProfilerActivity.CPU,
-                   torch.profiler.ProfilerActivity.CUDA,
-                ]
-                ) as p:
-                    benchmark(model, input_ids, check=args.check)
-                print(p.key_averages().table(sort_by="self_cuda_time_total", row_limit=-1))
-            else:
-                benchmark(model, input_ids, check=args.check)
+            benchmark(model, input_ids, check=args.check, torchprof=args.torch_profile)
 
     if args.eval:
         datasets = ['wikitext2', 'ptb', 'c4']
